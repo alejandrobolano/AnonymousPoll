8223d9d8dde58b83f683b56b4f756a5979ff4f24
Alejandro Bolaño <elbola.cuba1992@gmail.com>
2023-01-08 22:53:15 +0100

    test: Add new examples test about BusinessLogic and StudentLogic.

---
+++ AnonymousPoll.Test/BusinessLogicTest.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using AnonymousPoll.Business.Contracts;
+using AnonymousPoll.Business.Implementations;
+using AnonymousPoll.Common.Contracts;
+using AnonymousPoll.Common.Implementations;
+using AnonymousPoll.DataAccess.Contracts;
+using AnonymousPoll.DataAccess.Implementations;
+
+namespace AnonymousPoll.Test
+{
+    [TestClass]
+    public class BusinessLogicTest
+    {
+        private static IBusinessLogic _businessLogic;
+        private static IDataAccess _dataAccess;
+        private static IStudentLogic _studentLogic;
+        [ClassInitialize]
+        public static void Initialize(TestContext context)
+        {
+            _studentLogic = new StudentLogic();
+            _dataAccess = new TxtAccess(_studentLogic);
+            _businessLogic = new BusinessLogic(_dataAccess,_studentLogic);
+        }
+
+        [TestMethod()]
+        public void GetNamesAppendedTest_WhenListIsNullOrEmpty()
+        {
+            const string appendedExpected = "NONE";
+
+            #region null
+
+            var result = _businessLogic.GetNamesAppended(null);
+            Assert.AreEqual(appendedExpected, result.ToString());
+
+            #endregion
+
+            #region empty
+
+            var names = new List<string>();
+            result = _businessLogic.GetNamesAppended(names);
+            Assert.AreEqual(appendedExpected, result.ToString());
+
+            #endregion
+
+        }
+        [TestMethod()]
+        public void GetNamesAppendedTest()
+        {
+            var names = GetNamesHelper();
+            var result = _businessLogic.GetNamesAppended(names);
+            var appendedExpected = GetNamesExpectedHelper();
+            Assert.AreEqual(appendedExpected.ToString(), result.ToString());
+        }
+
+        #region helper
+        private static StringBuilder GetNamesExpectedHelper()
+        {
+            return new StringBuilder("alfa,beta,gamma");
+        }
+
+        private static List<string> GetNamesHelper()
+        {
+            return new List<string> { "beta", "gamma", "alfa" };
+        }
+        
+        #endregion
+    }
+}
---
+++ AnonymousPoll.Test/StudentLogicTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using AnonymousPoll.Common.Contracts;
+using AnonymousPoll.Common.Implementations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnonymousPoll.Test
+{
+    [TestClass()]
+    public class StudentLogicTests
+    {
+        private static IStudentLogic _studentLogic;
+
+        [ClassInitialize]
+        public static void Initialize(TestContext testContext)
+        {
+            _studentLogic = new StudentLogic();
+        }
+
+        [TestMethod()]
+        [DataRow("M,21,Human Resources Management,3")]
+        [DataRow("F,20,Systems Engineering,2")]
+        public void ConvertStringToStudentTest_WhenIsPartial(string stream)
+        {
+            var student = _studentLogic.ConvertStringToStudent(stream, true);
+            Assert.IsTrue(string.IsNullOrEmpty(student.Name));
+            Assert.IsTrue(student.Gender =='F' || student.Gender == 'M');
+            Assert.IsTrue(student.Age != 0);
+            Assert.IsTrue(!string.IsNullOrEmpty(student.Education));
+            Assert.IsTrue(student.AcademicYear != 0);
+        }
+
+        [TestMethod()]
+        [DataRow("Sophia Wright Torres,F,23,Radiologic Technology,1")]
+        [DataRow("Chloe Gomez Green,F,22,Diagnostic Radiography,2")]
+        public void ConvertStringToStudentTest(string stream)
+        {
+            var student = _studentLogic.ConvertStringToStudent(stream, false);
+            Assert.IsTrue(!string.IsNullOrEmpty(student.Name));
+            Assert.IsTrue(student.Gender =='F' || student.Gender == 'M');
+            Assert.IsTrue(student.Age != 0);
+            Assert.IsTrue(!string.IsNullOrEmpty(student.Education));
+            Assert.IsTrue(student.AcademicYear != 0);
+        }
+
+        [TestMethod()]
+        [DataRow("A,23,Radiologic Technology,1")]
+        [DataRow("F,23,Radiologic Technology,string")]
+        public void ConvertStringToStudentTest_WithError(string stream)
+        {
+            try
+            {
+                var student = _studentLogic.ConvertStringToStudent(stream, true);
+                Assert.Fail("Test has failed");
+            }
+            catch (FormatException exception)
+            {
+                Assert.IsTrue(exception.Message.Contains("Format incorrect of"));
+            }
+        }
+    }
+}
